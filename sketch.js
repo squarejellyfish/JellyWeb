fe1c786ba4813ca599c8c8ce0dd1e4a2c4b85fa0
jasonjustin <zxc3.zxc46@gmail.com>
2017-03-31 17:40:21 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var b;
+var bubble = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed(){
+  b = new Bubble(mouseX, mouseY);
+  bubble.push(b);
+}
+
+function mouseDragged(){
+  b = new Bubble(mouseX, mouseY);
+  bubble.push(b);
+}
+
+function draw() {
+  background(51);
+  for(var i = 0; i < bubble.length; i++){
+    bubble[i].show();
+    bubble[i].update();
+  }
+}
