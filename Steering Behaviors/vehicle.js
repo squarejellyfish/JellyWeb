22be01afcaabda35dafa72471bcea02a31823c76
jasonjustin <zxc3.zxc46@gmail.com>
2017-04-02 22:25:38 +0800

    Add files via upload

--- Steering Behaviors/vehicle.js
+++ Steering Behaviors/vehicle.js
@@ -2,15 +2,20 @@ function Vehicle(x, y) {
     this.pos = createVector(random(width), random(height));
     this.target = createVector(x, y);
     this.vel = createVector();
-    this.scc = createVector();
+    this.acc = createVector();
     this.r = 8;
-    this.maxspeed = 5;
-    this.maxforce = 0.3;
+    this.maxspeed = 10;
+    this.maxforce = 1;
 }
 
 Vehicle.prototype.behaviors = function() {
     var arrive = this.arrive(this.target);
+    var mouse = createVector(mouseX, mouseY);
+    var flee = this.flee(mouse);
     this.applyForce(arrive);
+    arrive.mult(1);
+    flee.mult(5);
+    this.applyForce(flee);
 }
 
 Vehicle.prototype.applyForce = function(f) {
@@ -36,8 +41,22 @@ Vehicle.prototype.arrive = function(target) {
     if (d < 100) {
         speed = map(d, 0, 100, 0, this.maxspeed);
     }
-    desired.setMag(this.maxspeed);
+    desired.setMag(speed);
     var steer = p5.Vector.sub(desired, this.vel);
     steer.limit(this.maxforce);
     return steer;
+}
+
+Vehicle.prototype.flee = function(target) {
+    var desired = p5.Vector.sub(target, this.pos);
+    var d = desired.mag();
+    if (d < 50) {
+        desired.setMag(this.maxspeed);
+        desired.mult(-1);
+        var steer = p5.Vector.sub(desired, this.vel);
+        steer.limit(this.maxforce);
+        return steer;
+    } else {
+        return createVector(0, 0);
+    }
 }
